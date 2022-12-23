a4141de8fa8bb0ffab821b583206703e2aeeca57
Henry Jao <jaohaohsuan@gmail.com>
2022-12-24 01:29:01 +1300

    init project, repo and apis

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+project_id = "terraform-in-action-ch7"
+namespace  = "team-ng"
+region     = "us-central1"
+
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "The GCP project id"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "namespace" {
+  description = "The project namespace to use for unique resource naming"
+  type        = string
+}
