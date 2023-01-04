12c4e2cc4108d5ef08e5a4236aade0fbecf2543e
Henry Jao <fivelinesofcode@gmail.com>
2023-01-04 14:50:19 +1300

    chore: add google-beta provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,8 +3,12 @@ terraform {
 
   required_providers {
     google = {
-        source = "hashicorp/google"
-        version = "~> 4.47.0"
+      source  = "hashicorp/google"
+      version = "~> 4.47.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.47.0"
     }
   }
 }
